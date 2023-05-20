e2b4444a7616f11738180f88848a94918b4241ac
r1ckshot <kapusticnyk.com@gmail.com>
2023-05-20 12:07:50 +0200

    Druga postac program typu Hello World, 3 pliki

--- hello.c
+++ hello.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-int main()
+void ph()
 {
     printf("Hello world!\n");
-    return 0;
 }
---
+++ print.c
@@ -0,0 +1,7 @@
+#include "print.h"
+
+int main()
+{
+    ph();
+    return 0;
+}
